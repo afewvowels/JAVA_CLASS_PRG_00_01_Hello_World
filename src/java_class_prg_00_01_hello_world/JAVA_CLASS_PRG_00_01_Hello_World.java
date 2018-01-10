737d836a0b548216303d4d559bffe6153122ed5c
kbsmith01 <kbsmith01@TS15860.ysu.local>
2018-01-10 12:19:16 -0500

    Initial project file.

---
+++ src/java_class_prg_00_01_hello_world/JAVA_CLASS_PRG_00_01_Hello_World.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_00_01_hello_world;
+
+/**
+ *
+ * @author kbsmith01
+ */
+public class JAVA_CLASS_PRG_00_01_Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
